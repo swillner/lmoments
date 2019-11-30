abf01f7553cba112ad14c638eeca4cb4c301a7ae
Sven Willner <sven.willner@gmail.com>
2019-11-30 21:11:45 +0100

    Style and performance improvements

--- include/gum.h
+++ include/gum.h
@@ -13,10 +13,12 @@ namespace lmoments {
 template<typename T>
 class GUM : public distribution<T> {
   protected:
-    T u = 0;
-    T a = 0;
+    T u;
+    T a;
 
   public:
+    explicit inline GUM(T u_p = 0, T a_p = 0) : u(u_p), a(a_p) {}
+
     template<typename DataVector = std::vector<T>>
     static GUM from_data(const DataVector& x) {
         GUM res;
@@ -32,8 +34,8 @@ class GUM : public distribution<T> {
     }
 
     inline void set_lmoment_ratios(const std::vector<T>& xmom) override {
-        static const T eu = .577215664901532861;   // Euler's Constant
-        static const T dl2 = .693147180559945309;  // log(2)
+        constexpr T eu = .577215664901532861;   // Euler's Constant
+        constexpr T dl2 = .693147180559945309;  // log(2)
         if (xmom[1] <= 0) {
             throw std::invalid_argument("lmoments invalid");
         }
@@ -42,11 +44,11 @@ class GUM : public distribution<T> {
     }
 
     inline void get_lmoment_ratios(std::vector<T>& xmom) const override {
-        const std::size_t nmom = xmom.size();
-        static const T zmom[20] = {.577215664901532861,   .693147180559945309,   .169925001442312363,   .150374992788438185,   .0558683500577583138,
-                                   .0581100239999710876,  .0276242584297309125,  .0305563766579053126,  .0164650282258328802,  .0187846624298170912,
-                                   .0109328215063027148,  .012697312667632953,   .00778982818057231804, .00914836179621999726, .00583332389328363588,
-                                   .00690104287590348154, .00453267970180679549, .00538916811326595459, .0036240776777236879,  .00432387608605538096};
+        const int nmom = xmom.size();
+        constexpr T zmom[20] = {.577215664901532861,   .693147180559945309,   .169925001442312363,   .150374992788438185,   .0558683500577583138,
+                                .0581100239999710876,  .0276242584297309125,  .0305563766579053126,  .0164650282258328802,  .0187846624298170912,
+                                .0109328215063027148,  .012697312667632953,   .00778982818057231804, .00914836179621999726, .00583332389328363588,
+                                .00690104287590348154, .00453267970180679549, .00538916811326595459, .0036240776777236879,  .00432387608605538096};
 
         if (nmom > 20) {
             throw std::invalid_argument("too many lmoment ratios requested");
@@ -55,7 +57,7 @@ class GUM : public distribution<T> {
         if (nmom > 1) {
             xmom[1] = a * zmom[1];
             if (nmom > 2) {
-                for (std::size_t j = 2; j < nmom; ++j) {
+                for (int j = 2; j < nmom; ++j) {
                     xmom[j] = zmom[j];
                 }
             }
--- include/utils.h
+++ include/utils.h
@@ -9,20 +9,20 @@ namespace lmoments {
 
 template<typename T>
 inline T dlgama(T x) {  // natural logarithm of the gamma function
-    static const T small = 1e-7;
-    static const T c5 = 8.41750841750841751e-4;
-    static const T c6 = -.00191752691752691753;
-    static const T c7 = .00641025641025641026;
-    static const T s1 = -.577215664901532861;  // -Euler's Constant
-    static const T s2 = .822467033424113218;   // pi^2/12
-    static const T crit = 13.;
-    static const T big = 1e9;
-    static const T toobig = 2e36;
-    static const T c0 = .918938533204672742;
-    static const T c1 = .0833333333333333333;
-    static const T c2 = -.00277777777777777778;
-    static const T c3 = 7.93650793650793651e-4;
-    static const T c4 = -5.95238095238095238e-4;
+    constexpr T small = 1e-7;
+    constexpr T c5 = 8.41750841750841751e-4;
+    constexpr T c6 = -.00191752691752691753;
+    constexpr T c7 = .00641025641025641026;
+    constexpr T s1 = -.577215664901532861;  // -Euler's Constant
+    constexpr T s2 = .822467033424113218;   // pi^2/12
+    constexpr T crit = 13.;
+    constexpr T big = 1e9;
+    constexpr T toobig = 2e36;
+    constexpr T c0 = .918938533204672742;
+    constexpr T c1 = .0833333333333333333;
+    constexpr T c2 = -.00277777777777777778;
+    constexpr T c3 = 7.93650793650793651e-4;
+    constexpr T c4 = -5.95238095238095238e-4;
 
     if (x <= 0 || x > toobig) {
         throw std::invalid_argument("argument out of range");
@@ -53,22 +53,22 @@ inline T dlgama(T x) {  // natural logarithm of the gamma function
     if (y >= big) {
         return sum1;
     }
-    T z__ = 1 / (y * y);
-    T sum2 = ((((((c7 * z__ + c6) * z__ + c5) * z__ + c4) * z__ + c3) * z__ + c2) * z__ + c1) / y;
+    const T z__ = 1 / (y * y);
+    const T sum2 = ((((((c7 * z__ + c6) * z__ + c5) * z__ + c4) * z__ + c3) * z__ + c2) * z__ + c1) / y;
     return sum1 + sum2;
 }
 
 template<typename T, typename DataVector = std::vector<T>>
 inline void lmoment_ratios_unbiased(const DataVector& x, std::vector<T>& xmom) {  // samlmu
-    const std::size_t nmom = xmom.size();
-    const int n = x.size();  // type must be signed
+    const int nmom = xmom.size();
+    const long n = x.size();  // type must be signed
     std::vector<T> coef1(nmom);
     std::vector<T> coef2(nmom);
 
     if (nmom > 100) {
         throw std::invalid_argument("too many lmoment ratios requested");
     }
-    for (std::size_t j = 0; j < nmom; ++j) {
+    for (int j = 0; j < nmom; ++j) {
         xmom[j] = 0.;
     }
     if (nmom <= 2) {
@@ -85,24 +85,24 @@ inline void lmoment_ratios_unbiased(const DataVector& x, std::vector<T>& xmom) {
             xmom[1] = sum2 / (n * (n - 1));
         }
     } else {
-        for (std::size_t j = 2; j < nmom; ++j) {
-            T temp = 1. / static_cast<T>(j * (n - j));
+        for (int j = 2; j < nmom; ++j) {
+            const T temp = 1. / static_cast<T>(j * (n - j));
             coef1[j] = (2 * j - 1) * temp;
             coef2[j] = ((j - 1) * (n + j - 1)) * temp;
         }
         T temp = -n - 1;
-        std::size_t nhalf = n / 2;
-        for (std::size_t i = 0; i < nhalf; ++i) {
+        const long nhalf = n / 2;
+        for (long i = 0; i < nhalf; ++i) {
             temp += 2.;
-            T xi = x[i];
-            T xii = x[n - 1 - i];
-            T termp = xi + xii;
-            T termn = xi - xii;
+            const T xi = x[i];
+            const T xii = x[n - 1 - i];
+            const T termp = xi + xii;
+            const T termn = xi - xii;
             xmom[0] += termp;
             T s1 = 1.;
             T s = temp / (n - 1);
             xmom[1] += s * termn;
-            for (std::size_t j = 2; j < nmom; j += 2) {
+            for (int j = 2; j < nmom; j += 2) {
                 T s2 = s1;
                 s1 = s;
                 s = coef1[j] * temp * s1 - coef2[j] * s2;
@@ -116,21 +116,21 @@ inline void lmoment_ratios_unbiased(const DataVector& x, std::vector<T>& xmom) {
             }
         }
         if (n % 2 == 1) {
-            T term = x[nhalf];
+            const T term = x[nhalf];
             T s = 1.;
             xmom[0] += term;
-            for (std::size_t j = 2; j < nmom; j += 2) {
+            for (int j = 2; j < nmom; j += 2) {
                 s = -coef2[j] * s;
                 xmom[j] += s * term;
             }
         }
         xmom[0] /= n;
         if (xmom[1] == 0.) {  // all data values are equal
-            for (std::size_t j = 2; j < nmom; ++j) {
+            for (int j = 2; j < nmom; ++j) {
                 xmom[j] = 0.;
             }
         } else {
-            for (std::size_t j = 2; j < nmom; ++j) {
+            for (int j = 2; j < nmom; ++j) {
                 xmom[j] /= xmom[1];
             }
             xmom[1] /= n;
